8a7965476af1745eb627963dcacb293d54432e58
Felix Brüll <felixbruell@live.de>
2019-03-26 11:10:46 +0900

    edited example

--- CppProperties/main.cpp
+++ CppProperties/main.cpp
@@ -7,11 +7,15 @@ class TestClass
 public:
 	TestClass()
 		:
-	fg(this, &TestClass::getVar),
-	fs(std::bind(&TestClass::setVar, this, std::placeholders::_1)),
-	fgs([this]() {return this->var; }, [this](int var) {this->var = var; })
+	GetOnly(this, &TestClass::getVar),
+	SetOnly(this, &TestClass::setVar),
+	FullProperty([this]() {return this->var; }, [this](int var) {this->var = var; })
 	{}
-private:
+
+	FunctionGetter<int> GetOnly;
+	FunctionSetter<int> SetOnly;
+	FunctionGetterSetter<int> FullProperty;
+
 	void setVar(int val)
 	{
 		var = val;
@@ -20,43 +24,27 @@ private:
 	{
 		return var;
 	}
-public:
-	FunctionGetter<int> fg;
-	FunctionSetter<int> fs;
-	FunctionGetterSetter<int> fgs;
 private:
 	int var = 0;
 };
 
 int main()
 {
-	//p1 = 1.0f;
-	//float val = p1;
+	DefaultGetter<float> DefGetter(10.0f);
+	float fval = DefGetter;
 
+	DefaultSetter<float> DefSetter(20.0f);
+	DefSetter = 15.0f;
 
-	DefaultGetter<float> dg(10.0f);
-	float fval = dg;
-	assert(fval == 10.0f);
-
-	DefaultSetter<float> ds(20.0f);
-	ds = 15.0f;
-
-	DefaultGetterSetter<int> dgs(20);
-	int ival = dgs;
-	dgs.DefaultSetter<int>::operator=(200);
-	assert(ival == 20);
-	dgs = 200;
-	assert(dgs == 200);
+	DefaultGetterSetter<int> DefProperty(20);
+	int ival = DefProperty;
+	DefProperty = 200;
 
 	TestClass tc;
-
-	ival = tc.fg;
-	assert(ival == 0);
-
-	tc.fs = 30;
-	assert(tc.fg == 30);
-
-	tc.fgs = 40;
+	ival = tc.GetOnly;
+	tc.SetOnly = 30;
+	tc.FullProperty = 40;
+	ival = tc.FullProperty;
 
 	return 0;
 }
